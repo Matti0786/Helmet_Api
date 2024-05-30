27f8cc8c724be46eec0f95661d7fb8d2a4ee1a4e
matee <mateeullah0060@gmail.com>
2024-05-30 07:43:00 +0100

    change in main

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import io
 import base64
 import numpy as np
 import json
-
+import os
 
 app = Flask(__name__)
 
@@ -54,4 +54,6 @@ def upload_image():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
